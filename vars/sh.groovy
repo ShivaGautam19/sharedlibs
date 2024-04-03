5f390c67b7845a4dd8da60b62d817558ec981d7a
ShivaGautam19 <163097148+ShivaGautam19@users.noreply.github.com>
2024-04-03 14:16:15 +0530

    Update sh.groovy

--- vars/sh.groovy
+++ vars/sh.groovy
@@ -1,3 +1,4 @@
-def call(mvn, options){
-  sh "${mvn} ${options}"
-}  
+def call(String mvnName, String options = '') {
+    def mvnHome = tool mvnName
+    sh "${mvnHome}/bin/mvn -B compile ${options}"
+} 
